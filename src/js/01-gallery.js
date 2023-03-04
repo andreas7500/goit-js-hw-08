8fd9ec1bffbcb51a3766a8b2c1ae9a609cc2f159
andreas7500 <andreas2875@gmail.com>
2023-03-05 01:06:16 +0200

    gallery commit1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const div = document.querySelector('.gallery');
+const galleryRender = galleryItems =>
+  galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`,
+    ''
+  );
+
+const insertImages = string => {
+  div.insertAdjacentHTML('beforeend', string);
+};
+
+const results = galleryRender(galleryItems);
+insertImages(results);
+
+// 777777777777777777
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
