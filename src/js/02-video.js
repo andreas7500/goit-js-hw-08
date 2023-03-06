3a5aa2c73eb23eb9307b4e69f4823a253a0178b5
andreas7500 <andreas2875@gmail.com>
2023-03-06 23:01:19 +0200

    first final commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const VIM_KEY = 'videoplayer-current-time';
+
+function onPlay({ seconds }) {
+  localStorage.setItem(VIM_KEY, seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(VIM_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const VALUE_KEY = 'feedback-form-state';
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
 
+// hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+popularMessage();
+const formData = {};
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(VALUE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem(VALUE_KEY)));
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill the fields!');
+  }
+  e.currentTarget.reset();
+  localStorage.removeItem(VALUE_KEY);
+}
+
+function popularMessage() {
+  const saveMessage = JSON.parse(localStorage.getItem(VALUE_KEY));
+  const message = document.querySelector('.feedback-form textarea');
+  const email = document.querySelector('.feedback-form input');
+  if (saveMessage) {
+    console.log(saveMessage);
+    message.value = saveMessage.message;
+    email.value = saveMessage.email;
+  }
+}
